6e8b213d52cd672cc3fe32b4efe856f9b413e772
Samuel Barbosa <samuelesdras@yahoo.com.br>
2024-07-18 18:52:09 -0300

    solving hydratation problem on in useLocalStore hoook

--- src/hooks/useLocalStore.ts
+++ src/hooks/useLocalStore.ts
@@ -1,28 +1,16 @@
 import { useState, useEffect } from "react";
 
 export function useLocalStorage<T>(key: string, initialValue: T) {
-  const [storedValue, setStoredValue] = useState<T>(() => {
-    if (typeof window !== "undefined") {
-      try {
-        const item = window.localStorage.getItem(key);
-        return item ? JSON.parse(item) : initialValue;
-      } catch (error) {
-        console.error(error);
-        return initialValue;
-      }
-    }
-    return initialValue;
-  });
+  const [storedValue, setStoredValue] = useState<T | undefined>(undefined);
 
   useEffect(() => {
     if (typeof window !== "undefined") {
       try {
         const item = window.localStorage.getItem(key);
-        if (item !== null) {
-          setStoredValue(JSON.parse(item));
-        }
+        setStoredValue(item ? JSON.parse(item) : initialValue);
       } catch (error) {
         console.error(error);
+        setStoredValue(initialValue);
       }
     }
   }, [key]);
@@ -30,7 +18,7 @@ export function useLocalStorage<T>(key: string, initialValue: T) {
   const setValue = (value: T | ((val: T) => T)) => {
     try {
       const valueToStore =
-        value instanceof Function ? value(storedValue) : value;
+        value instanceof Function ? value(storedValue ?? initialValue) : value;
       setStoredValue(valueToStore);
       if (typeof window !== "undefined") {
         window.localStorage.setItem(key, JSON.stringify(valueToStore));
